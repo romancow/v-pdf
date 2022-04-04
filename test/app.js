711c10fa0a9a8f79061deb9ad08726249d50d51e
Sean Wilson <seancwilson@gmail.com>
2022-04-04 17:38:30 -0500

    Add some test files

---
+++ test/app.js
@@ -0,0 +1,42 @@
+import { VPdfPage as VPdf } from "../build/v-pdf.esm.js"
+	
+var app = new Vue({
+	components: { VPdf },
+	el: '#app',
+	data: {
+		title: "VPdf Test",
+		pdf: "file-sample.pdf",
+		currentPage: 1,
+		lastPage: 1
+
+	},
+	computed: {
+		isLastPage() {
+			const { currentPage, lastPage } = this
+			return currentPage === lastPage
+		},
+
+		isFirstPage() {
+			return this.currentPage <= 1
+		}
+	},
+	methods: {
+		documentLoad({ document }) {
+			this.lastPage = document.numPages
+		},
+
+		prevPage() {
+			const { currentPage } = this
+			this.currentPage = Math.max(currentPage - 1, 1)
+		},
+
+		nextPage() {
+			const { currentPage, lastPage } = this
+			this.currentPage = Math.min(currentPage + 1, lastPage)
+		},
+
+		logError(error) {
+			console.error(error)
+		}
+	}
+})
